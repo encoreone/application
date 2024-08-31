4b9a1114c812c1c436d109ad3c4b8849ce98d66c
Encore <encore@Encore-2.local>
2024-08-31 07:00:21 +0300

    fix release

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import { createRoot } from 'react-dom/client';
 
 import App from './app/ui/index.tsx';
 
-const rootElement = document.getElementById('root')!;
+import './mockEnv.ts';
 
 import '@telegram-apps/telegram-ui/dist/styles.css';
+
+const rootElement = document.getElementById('root')!;
 
 createRoot(rootElement).render( <App /> );
