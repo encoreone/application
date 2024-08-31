c1ef82fc4fd42e144bc87094cb25bd598d5ed6a9
Encore <encore@Encore-2.local>
2024-08-31 07:43:45 +0300

    fix

--- src/widgets/IndexPage/ui/index.tsx
+++ src/widgets/IndexPage/ui/index.tsx
@@ -1,14 +1,19 @@
-import { Section, Cell, Image, List } from '@telegram-apps/telegram-ui';
+import { Section, Cell, Image, List, Button, Modal, Placeholder } from '@telegram-apps/telegram-ui';
 import type { FC } from 'react';
 
 import { Link } from '../../../shared/ui/Link/ui';
+import { ModalHeader } from '@telegram-apps/telegram-ui/dist/components/Overlays/Modal/components/ModalHeader/ModalHeader';
 
 export const IndexPage: FC = () => {
+
+
+
+
   return (
     <List>
       <Section
-        header='Features'
-        footer='You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects'
+        header='Депнуть в ракетку через тон валлет'
+        footer=''
       >
         <Link to='/ton-connect'>
           <Cell
@@ -20,18 +25,45 @@ export const IndexPage: FC = () => {
         </Link>
       </Section>
       <Section
-        header='Application Launch Data'
-        footer='These pages help developer to learn more about current launch information'
+        header='Обязательные кнопки'
+        footer=''
       >
-        <Link to='/init-data'>
-          <Cell subtitle='User data, chat information, technical data'>Init Data</Cell>
+        <Link to='/'
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'nowrap',
+          alignContent: 'center',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+      
+        <Modal
+          header={<ModalHeader>Вывод средств</ModalHeader>}
+          trigger={<Button size="m">Вывести</Button>}
+        >
+        <Placeholder
+          description="Для начала нужно депнуть"
+          header="Что-то пошло не так"
+        >
+          <img
+            alt="Telegram sticker"
+            src="https://xelene.me/telegram.gif"
+            style={{
+              display: 'block',
+              height: '144px',
+              width: '144px'
+            }}
+          />
+        </Placeholder>
+      </Modal>
         </Link>
-        <Link to='/launch-params'>
+        {/* <Link to='/launch-params'>
           <Cell subtitle='Platform identifier, Mini Apps version, etc.'>Launch Parameters</Cell>
         </Link>
         <Link to='/theme-params'>
           <Cell subtitle='Telegram application palette information'>Theme Parameters</Cell>
-        </Link>
+        </Link> */}
       </Section>
     </List>
   );
